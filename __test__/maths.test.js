0d9e973a1504c704357471f2c303298bc481e918
Luis Alfredo Porras Paez <luis@webdox.cl>
2021-07-07 17:39:52 -0400

    testing maths

--- __test__/maths.test.js
+++ __test__/maths.test.js
@@ -4,4 +4,13 @@ describe('Calculos matemáticos', () => {
   test('Prueba de sumas', () => {
     expect(sumar(1, 1)).toBe(2);
   });
+  test('Multiplicar', () => {
+    expect(multiplicar(2, 2)).toBe(4);
+  });
+  test('Resta', () => {
+    expect(restar(3, 2)).toBe(1);
+  });
+  test('Dividir', () => {
+    expect(dividir(10, 2)).toBe(5);
+  });
 });
