68f9a23c68035e59af900349cd8224cfe8aae03d
Luis Alfredo Porras Paez <luis@webdox.cl>
2021-07-21 18:35:26 -0400

    making a failing test with random data

--- __test__/snapshot.test.js
+++ __test__/snapshot.test.js
@@ -1,8 +1,17 @@
 import { getCharacter } from "../snapshot";
 import rick from '../rick.json';
+import { expect, test } from "@jest/globals";
 
 describe('Es hora de las instantaneas', () => {
   test('Snapshot', () => {
     expect(getCharacter(rick)).toMatchSnapshot();
+  });
+
+  test('Siempre fallará la instantanea', () => {
+    const user = {
+      createdAt: new Date(),
+      id: Math.floor(Math.random() * 20),
+    };
+    expect(user).toMatchSnapshot();
   });
 });
